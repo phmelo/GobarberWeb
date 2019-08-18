f6d54ce459de712bffb0fc222ea63f78d61bc3e6
Paulo <phmelomorais@gmail.com>
2019-08-17 23:00:16 -0300

    feature:Header

--- src/pages/_layouts/default/index.js
+++ src/pages/_layouts/default/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Header from '~/components/Header';
+
 import { Wrapper } from './styles';
 
 export default function DefaultLayout({ children }) {
-  return <Wrapper>{children}</Wrapper>;
+  return (
+    <Wrapper>
+      <Header />
+      {children}
+    </Wrapper>
+  );
 }
 
-DefaultLayout.prototype = {
+DefaultLayout.propTypes = {
   children: PropTypes.element.isRequired,
 };
